68a00df1e441d226a6ecbcde8d19c74051ca029a
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-07-21 16:35:04 -0700

    Typo corrections.

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const path = require("path");
 const fs = require("fs");
 
-// const normalizedPath = path.join(__dirname, "routes");
-
 fs.readdirSync(__dirname).forEach(function(file) {
   if (file != "index.js") {
     router.use("/", require("./" + file));
